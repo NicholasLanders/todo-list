452ef9f9bc3958d8cd1d90cf1b977a5161f9bd1d
Nicholas Landers <nicholaslanders1234@gmail.com>
2025-11-24 20:52:36 -0500

    Working on the to do list portion (as opposed to the lists) and this current functionality works in a way that when someone types an item into the box, and then clicks away it will create a new item below so that they can then create another item. I am thinking I am going to make it work more like google tasks, where there is a dedicated button for a new task, as I don't like the way it looks. May revert back to this depending. Also added some margin to the item so that there is more spacing in between each one.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 const todoInput = document.querySelector(".todo-text") as HTMLInputElement;
 const todoItem = document.querySelector(".todo-item") as HTMLElement;
-const clone = todoItem.cloneNode(true) as HTMLElement;
 const main = document.querySelector("main");
 
-todoInput?.addEventListener("input", () => {
-    if (todoInput.value !== "Task") {
-        main?.appendChild(clone);
+let clones: HTMLElement[] = [];
+//TODO: Need to think about it all when it comes to deleting elements in different orders of
+//      the list and such
+main?.addEventListener("change", (e) => {
+    const target = e.target;
+    if (!(target instanceof HTMLInputElement)) return;
+    if (!target.classList.contains("todo-text")) return;
+
+    if (target.value !== "") {
+        const newClone = todoItem.cloneNode(true) as HTMLElement;
+        newClone.querySelector<HTMLInputElement>(".todo-text")!.value = "";
+        main?.appendChild(newClone);
+        clones.push(newClone);
+    } else {
+        const lastClone = clones.pop();
+        main?.removeChild(lastClone!);
     }
 });

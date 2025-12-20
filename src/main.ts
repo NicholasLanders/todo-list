3b387607c7ed1dc3c29941f298a99dd181199260
Nicholas Landers <nicholaslanders1234@gmail.com>
2025-12-19 23:34:23 -0500

    Changed the order of the code so that the list stuff is first, and then the todo item stuff is after. So working on the implementation of picking the different todo lists. This involves creating a map where we are mapping the list node to an array of todo nodes. I also am made a separate function for the deletion of elements, as the first element and the ones after need to do the same action. Will need to do this for the lists as well.

--- src/main.ts
+++ src/main.ts
@@ -5,41 +5,57 @@ const listItem = document.querySelector(".list-item");
 const listButton = document.querySelector("#list-button");
 const listItemEntry = document.querySelector("#list-entry");
 
-const initialTodoDelete = todoItem?.querySelector(".todo-delete");
-initialTodoDelete?.addEventListener("click", () => {
-    initialTodoDelete.parentElement?.parentElement?.remove();
-});
-
-let todoClones: HTMLElement[] = [];
-todoButton?.addEventListener("click", () => {
-    const newTodoItem = todoItem?.cloneNode(true) as HTMLElement;
-    newTodoItem.querySelector<HTMLInputElement>(".todo-text")!.value = "";
-    todoClones.push(newTodoItem);
-    todoItemEntry?.after(newTodoItem);
-
-    const dltBtn = newTodoItem.querySelector(".todo-delete");
-    dltBtn?.addEventListener("click", () => {
-        dltBtn.parentElement?.parentElement?.remove();
-
-        const btnIndex = todoClones.indexOf(newTodoItem);
-        if (btnIndex !== -1) todoClones.splice(btnIndex, 1);
-    });
-});
-
+//Delete button on first element
 const initialListDelete = listItem?.querySelector(".list-delete");
 initialListDelete?.addEventListener("click", () => {
     initialListDelete.parentElement?.remove();
 });
 
+const lists = new Map<HTMLElement, HTMLElement[]>();
 listButton?.addEventListener("click", () => {
     const newListItem = listItem?.cloneNode(true) as HTMLElement;
+    lists.set(newListItem, []);
     newListItem.querySelector<HTMLInputElement>(".list-text")!.value = "";
     const radio = newListItem.querySelector<HTMLInputElement>(".list-select");
     if (radio) radio.checked = false;
     listItemEntry?.after(newListItem);
 
+    //TODO: Need to delete the associated list when it is deleted
     const dltBtn = newListItem.querySelector(".list-delete");
     dltBtn?.addEventListener("click", () => {
         dltBtn.parentElement?.remove();
     });
 });
+
+//Delete button on first element
+const firstTodoDltBtn = todoItem?.querySelector(".todo-delete");
+firstTodoDltBtn?.addEventListener("click", () =>
+    deleteTodo(firstTodoDltBtn as HTMLElement, todoItem as HTMLElement)
+);
+
+let curListItem = document.querySelector(
+    ".list-item input[type='radio']:checked"
+);
+lists.set(curListItem as HTMLElement, [todoItem as HTMLElement]);
+todoButton?.addEventListener("click", () => {
+    const newTodoItem = todoItem?.cloneNode(true) as HTMLElement;
+    newTodoItem.querySelector<HTMLInputElement>(".todo-text")!.value = "";
+    curListItem = document.querySelector(
+        ".list-item input[type='radio']:checked"
+    );
+    lists.get(curListItem as HTMLElement)?.push(newTodoItem);
+    todoItemEntry?.after(newTodoItem);
+
+    const dltBtn = newTodoItem.querySelector(".todo-delete");
+    dltBtn?.addEventListener("click", () =>
+        deleteTodo(dltBtn as HTMLElement, newTodoItem)
+    );
+});
+
+function deleteTodo(dltBtn: HTMLElement, newTodoItem: HTMLElement) {
+    dltBtn.parentElement?.parentElement?.remove();
+
+    const curTodoArray = lists.get(curListItem as HTMLElement);
+    const btnIndex = curTodoArray?.indexOf(newTodoItem) as number;
+    if (btnIndex !== -1) curTodoArray?.splice(btnIndex, 1);
+}
